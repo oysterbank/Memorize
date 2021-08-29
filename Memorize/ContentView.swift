3f8dd4cdbaecc94791f6298df0877fe3a0e83592
Kris Laratta <krislaratta@gmail.com>
2021-08-28 21:12:08 -0700

    UI reacts to model changes, flip cards

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,93 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var emojis = ["🚝", "✈️", "🚤", "🛰", "🚘", "🛳", "🚡", "🚞", "🚎", "🚲", "🚁", "🛸"]
-    @State var emojiCount = 12
+    @ObservedObject var viewModel: EmojiMemoryGame
 
     var body: some View {
-        VStack {
-            Text("Memorize!").font(.largeTitle)
-            ScrollView {
-                LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
-                    ForEach(emojis[0..<emojiCount], id: \.self)  { emoji in
-                        CardView(content: emoji)
-                            .aspectRatio(2/3, contentMode: .fit)
-                    }
+        ScrollView {
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
+                ForEach(viewModel.cards)  { card in
+                    CardView(card: card)
+                        .aspectRatio(2/3, contentMode: .fit)
+                        .onTapGesture {
+                            viewModel.choose(card)
+                        }
                 }
             }
-            .foregroundColor(.purple)
-            Spacer()
-            HStack(alignment: .bottom) {
-                vehicles
-                Spacer()
-                animals
-                Spacer()
-                plants
-            }
-            .font(.largeTitle)
-            .padding(.horizontal)
         }
-        .padding(.horizontal)
-    }
-    
-    var vehicles: some View {
-        Button {
-            emojis = ["🚝", "✈️", "🚤", "🛰", "🚘", "🛳", "🚡", "🚞", "🚎", "🚲", "🚁", "🛸"].shuffled()
-            emojiCount = Int.random(in: 4...emojis.count)
-        } label: {
-            VStack {
-                Image(systemName: "car")
-                Text("Vehicles")
-                    .font(.caption)
-            }
-        }
-        .padding(.horizontal)
-    }
-    var animals: some View {
-        Button {
-            emojis = ["🐶", "🐰", "🦊", "🐼", "🐸", "🐦", "🐌", "🐙", "🦆", "🦕", "🐠", "🐒", "🐮", "🐪", "🐋", "🐉"].shuffled()
-            emojiCount = Int.random(in: 4...emojis.count)
-        } label: {
-            VStack {
-                Image(systemName: "ladybug")
-                Text("Animals")
-                    .font(.caption)
-            }
-        }
-        .padding(.horizontal)
-    }
-    var plants: some View {
-        Button {
-            emojis = ["🌴", "🌲", "☘️", "💐", "🌾", "🌻", "🌳", "🌵"].shuffled()
-            emojiCount = Int.random(in: 4...emojis.count)
-        } label: {
-            VStack {
-                Image(systemName: "leaf")
-                Text("Plants")
-                    .font(.caption)
-            }
-        }
+        .foregroundColor(.purple)
         .padding(.horizontal)
     }
 }
 
 struct CardView: View {
-    var content: String
-    @State var isFaceUp: Bool = true
-    
+    let card: MemoryGame<String>.Card
     var body: some View {
         ZStack {
             let shape = RoundedRectangle(cornerRadius: 20)
-            if isFaceUp {
+            if card.isFaceUp {
                 shape.fill().foregroundColor(.white)
                 shape.strokeBorder(lineWidth: 3)
-                Text(content).font(.largeTitle)
+                Text(card.content).font(.largeTitle)
             } else {
                 shape.fill()
             }
-        }
-        .onTapGesture {
-            isFaceUp = !isFaceUp
         }
     }
 }
@@ -123,9 +67,10 @@ struct CardView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        let game = EmojiMemoryGame()
+        ContentView(viewModel: game)
             .preferredColorScheme(.dark)
-        ContentView()
+        ContentView(viewModel: game)
             .preferredColorScheme(.light)
     }
 }
--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-class EmojiMemoryGame {
+class EmojiMemoryGame: ObservableObject {
     static let emojis = ["🚝", "✈️", "🚤", "🛰", "🚘", "🛳", "🚡", "🚞", "🚎", "🚲", "🚁", "🛸"]
     
     static func createMemoryGame() -> MemoryGame<String> {
@@ -16,9 +16,15 @@ class EmojiMemoryGame {
         }
     }
     
-    private var model: MemoryGame<String> = createMemoryGame()
+    @Published private var model: MemoryGame<String> = createMemoryGame()
     
     var cards: Array<MemoryGame<String>.Card> {
-        return model.cards
+        model.cards
+    }
+    
+    // MARK: - Intent(s)
+    
+    func choose(_ card: MemoryGame<String>.Card) {
+        model.choose(card)
     }
 }
